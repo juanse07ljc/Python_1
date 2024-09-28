507b93f59968a90986a4c29a87f2e13edda206ab
sebastian <juanse07@gmail.com>
2024-09-27 19:24:05 -0500

    mi segundo commit

---
+++ Nombre a una funcion.py
@@ -0,0 +1,9 @@
+###luego le asigno el nombre a la función
+#Dentro de parentesis pongo mis parametros, los cuales son los valores que voy a reciibir
+#para operarlos dentro de mi función y ponemos don puntos
+def add_two_numbers(number_one, number_two):
+###Declaramos una variable que se llama total y decimos que va a ser igual a la suma de mis
+###parametros
+  total = number_one + number_two
+###Imprimo con la palabra reservada print la variable total la cual debe ir entre parentesis
+  print("La suma de los parametros es: ", total)
